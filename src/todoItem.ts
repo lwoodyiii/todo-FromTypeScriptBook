b02d707383b92b0dca3cdb0bbd7c00395bb55d3f
Leonard Woody <leonard.woody@gmail.com>
2020-03-08 20:29:46 -0400

    took out unneeded code

--- src/todoItem.ts
+++ src/todoItem.ts
@@ -4,12 +4,9 @@ export class TodoItem {
     public complete: boolean = false;
 
     public constructor(id: number, task: string, complete: boolean = false){
-        this.id = id;
-        this.task = task;
-        this.complete = complete;
     }
 
-    public printDetails() : void {
-        console.log('${this.id|\t${this.task} ${this.complete ? "\t(complete)":""}';
+    printDetails() : void {
+        console.log('${this.id|\t${this.task} ${this.complete ? "\t(complete)":""}');
     }
 }
